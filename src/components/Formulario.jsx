2a582fa0e8b8284aa13eae131eea3bd276231454
David <davidramirez9210@gmail.com>
2021-05-06 14:56:01 -0300

    Finishing proyect

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import styles from './Formulario.module.css';
 import useSelect from '../hooks/useSelect';
+import PropTypes from 'prop-types'
 
-const Formulario = () => {
+const Formulario = ({guardarCategoria}) => {
 
 const OPCIONES = [
     {value: 'general',label: 'General'},
@@ -16,16 +17,24 @@ const OPCIONES = [
 
 //utilizar custom hook
     const [categoria,SelectNoticias] = useSelect('general',OPCIONES);
+
+    //submit al form, pasar categoria a app.js
+    const buscarNoticias = e => {
+        e.preventDefault();
+        guardarCategoria(categoria);
+    }
     return ( 
         <div className={`${styles.buscador} row`}>
             <div className="col s12 m8 offset-m2">
-                <form>
+                <form
+                    onSubmit={buscarNoticias}
+                >
                     <h2 className={styles.heading}>Encuentra Noticias por Categoria</h2>
                     <SelectNoticias/>
 
                     <div className="input-field col s12">
                         <input 
-                            tipe="submit"
+                            type="submit"
                             className={`${styles.btn_block} btn-large amber darken-2`}
                             value="Buscar" 
                         />
@@ -35,5 +44,7 @@ const OPCIONES = [
         </div>
      );
 }
- 
+ Formulario.propTypes={
+    guardarCategoria:PropTypes.func.isRequired
+ }
 export default Formulario;
